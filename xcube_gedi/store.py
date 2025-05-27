a6b94192530ba0e7990ad0f52dae367bf622700c
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2025-05-27 19:43:57 +0200

    initial basic impl

--- xcube_gedi/store.py
+++ xcube_gedi/store.py
@@ -24,21 +24,25 @@ from abc import ABC
 from typing import Tuple, Iterator, Container, Union, Any
 
 import xarray as xr
-from xcube.core.store import (
-    DataDescriptor,
-    DataStore,
-    DataTypeLike
-)
+from xcube.core.store import DataDescriptor, DataStore, DataTypeLike
 from xcube.util.jsonschema import (
     JsonObjectSchema,
 )
+
+import gedidb as gdb
+
+from xcube_gedi.constant import GEDI_S3_BUCKET_NAME, GEDI_URL
 
 _LOG = logging.getLogger("xcube.gedi")
 
 
 class GediDataStore(DataStore, ABC):
-    """ Implementation of Gedi data store .
-    """
+    """Implementation of Gedi data store ."""
+
+    def __init__(self):
+        self.provider = gdb.GEDIProvider(
+            storage_type="s3", s3_bucket=GEDI_S3_BUCKET_NAME, url=GEDI_URL
+        )
 
     @classmethod
     def get_data_store_params_schema(cls) -> JsonObjectSchema:
@@ -52,9 +56,9 @@ class GediDataStore(DataStore, ABC):
         raise NotImplementedError
 
     def get_data_ids(
-            self,
-            data_type: DataTypeLike = None,
-            include_attrs: Container[str] | bool = False,
+        self,
+        data_type: DataTypeLike = None,
+        include_attrs: Container[str] | bool = False,
     ) -> Union[Iterator[str], Iterator[tuple[str, dict[str, Any]]]]:
         raise NotImplementedError
 
@@ -62,7 +66,7 @@ class GediDataStore(DataStore, ABC):
         raise NotImplementedError
 
     def describe_data(
-            self, data_id: str, data_type: DataTypeLike = None
+        self, data_id: str, data_type: DataTypeLike = None
     ) -> DataDescriptor:
         raise NotImplementedError
 
@@ -77,16 +81,37 @@ class GediDataStore(DataStore, ABC):
         raise NotImplementedError
 
     def open_data(
-        self, data_id: str, opener_id: str = None, **open_params
+        self, data_id: str = None, opener_id: str = None, **open_params
     ) -> xr.Dataset:
-        raise NotImplementedError
+        vars_selected = ["agbd", "rh"]
+        return self.provider.get_data(
+            variables=vars_selected,
+            query_type="nearest",
+            point=(8.948581, 50.716982),
+            num_shots=5,
+            radius=0.1,
+            start_time="2023-01-01",
+            end_time="2024-07-02",
+            return_type="xarray",
+        )
 
     def get_search_params_schema(
-            cls, data_type: DataTypeLike = None
+        cls, data_type: DataTypeLike = None
     ) -> JsonObjectSchema:
         raise NotImplementedError
 
     def search_data(
-            self, data_type: DataTypeLike = None, **search_params
+        self, data_type: DataTypeLike = None, **search_params
     ) -> Iterator[DataDescriptor]:
         raise NotImplementedError
+
+
+# TODO:
+#  1. Clarify what to use for data_ids as we can only access the
+#  variables through the gedidb api.
+#  2. What to do for list_data_ids()? Do we return variables or do we need a
+#  new method for it? Does it seem it is deviating from the datastore
+#  interface?
+#  3. Although the dataset that is returned is xarray, its dimensions are
+#  shot_number and profile_points. Need to clarify with stakeholders if that
+#  is okay or not.s
